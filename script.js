b7b6692cf6598ca928d0b8e989716eef76d12a21
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2024-12-25 03:52:10 -0800

    Add library table and more functions

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
 const myLibrary = [];
+const libraryTable = document.querySelector("table");
+
+const dialog = document.querySelector("dialog");
+const showFormButton = document.querySelector(".show-form");
+showFormButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+const submitFormButton = document.querySelector(".submit-form");
+submitFormButton.addEventListener("click", () => {
+    
+    const form = querySelector("form");
+    const formData = new FormData(form);
+
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("pages")
+    const read = formData.has("read") ? "Read" : "Not Read";
+    addBook(title, author, pages, read); 
+});
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return this.title + " by " + this.author + ", " + this.pages + " pages, " + read;
-    }
+    this.read = read; 
+}
+
+Book.prototype.info = function() {
+    return [this.title, this.author, this.pages, this.read];
 }
 
 function addBook(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
 
-function showBooks() {
-    myLibrary.forEach(book => {
-        console.log(book);
+function clearBooks() {
+    const booksInfo = document.querySelectorAll(".book-info");
+    booksInfo.forEach(bookInfo => {
+        bookInfo.remove();
     });
 }
 
-addBook("Harry Potter", "Rowling", 1000, "read");
-addBook("Naruto", "Kishimoto", 350, "not read");
-showBooks();
-addBook("To Kill A Mockingbird", "Lee", 500, "read");
+function showBooks() {
+    myLibrary.forEach(book => {
+        const newRow = document.createElement("tr");
+        newRow.classList.add("book-info");
+        libraryTable.appendChild(newRow);
+        
+        book.info().forEach(field => {
+            const newCell = document.createElement("td");
+            newCell.textContent = field;
+            newRow.appendChild(newCell);
+        });
+    });
+}
 
+addBook("Harry Potter", "Rowling", 1000, "Yes");
+addBook("Naruto", "Kishimoto", 350, "No");
+showBooks();
+addBook("To Kill A Mockingbird", "Lee", 500, "Yes");
 showBooks();
